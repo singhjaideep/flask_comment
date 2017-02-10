54626b68f9d7efbefc88542aa1be56f39433e225
Jaideep Singh <jaideep.singh@morningstar.com>
2017-02-10 13:28:59 -0600

    Added MySQL connection string

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,22 @@
-
 # A very simple Flask Hello World app for you to get started with... https://blog.pythonanywhere.com/121/
-
 from flask import Flask, redirect, render_template, request, url_for
-
+from flask.ext.sqlalchemy import SQLAlchemy
+import secrets
 app = Flask(__name__)
 app.config["DEBUG"] = True
-comments = []
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username=secrets.username,
+    password=secrets.password,
+    hostname=secrets.hostname,
+    databasename=secrets.databasename,
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
+db = SQLAlchemy(app)
+
+comments =[]
 
 @app.route("/", methods=["GET", "POST"])
 def index():
