48d300346efc90a4a5706f03b00f9e295110efb6
Jaideep Singh <jaideep.singh@morningstar.com>
2017-02-10 13:42:59 -0600

    Adding table

--- flask_app.py
+++ flask_app.py
@@ -16,6 +16,13 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 
 db = SQLAlchemy(app)
 
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+    #commentsectionid for future use
 comments =[]
 
 @app.route("/", methods=["GET", "POST"])
