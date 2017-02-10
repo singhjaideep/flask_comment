ff0f89343cb7ba07949e43b8e2e7d2c72f2a596e
Jaideep Singh <jaideep.singh@morningstar.com>
2017-02-10 13:48:27 -0600

    Added comments to db

--- flask_app.py
+++ flask_app.py
@@ -23,13 +23,14 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
     #commentsectionid for future use
-comments =[]
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=comments)
+        return render_template("main_page.html", comments=Comment.query.all())
 
-    comments.append(request.form["contents"])
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
     return redirect(url_for('index'))
 
